64d186a7a8d33ae91d022fb01a0e717e743b8555
Usman <74776275+usmanmateen@users.noreply.github.com>
2022-11-09 23:58:17 +0000

    Added two functions one of them checks if the parameters are correct if they are correct then the second function returns the total estimated flight time between the two airports. Parameters are the IATA codes of airports for example BHX for Birmingham.

--- new.py
+++ new.py
@@ -117,7 +117,10 @@ def map(lat,lon="0.00,0.00"):
     
   except:
     print('')
-map(x[0]["location"])
+try:
+  map(x[0]["location"])
+except:
+  print('')
 
 
 
@@ -230,4 +233,43 @@ def message(time="00:00",flight_insight=x):
   print(message.sid)
 
 message(x[0]["departure"]["scheduledTimeLocal"],x[0])
+ 
 
+  
+def airport_code(airport = 'bhx',airport2 = 'lhr'):
+  airport = str(input("3 digits airport code. Example: Bhx for Brirmingham > "))
+  airport2 = str(input('3 digits airport code. Example:  LHR for London > '))
+  
+  if len(airport) == 3 and len(airport2) == 3:
+    return airport, airport2
+    
+  else:
+    return print(False)
+
+
+
+def airportDistance():
+  
+  list_arpt = list(airport_code())
+  airport = list_arpt[0]
+  airport2 = list_arpt[1]
+
+  
+  url = 'https://aerodatabox.p.rapidapi.com/airports/iata/'
+
+  urlFinal = url + airport + "/distance-time/"+ airport2
+
+  headers = {
+	  "X-RapidAPI-Key": "7fd9c4ad4fmshd8f66ec397141d0p135583jsnc53fe7e95b0f",
+	  "X-RapidAPI-Host": "aerodatabox.p.rapidapi.com"
+  }
+
+  response = requests.request("GET", urlFinal, headers=headers)
+
+  approx = response.json()["approxFlightTime"]
+
+  return print('Estimated flight time from ' +airport + ' to ' + airport2 +' is ' +approx)
+
+airportDistance()
+  
+  
---
+++ test.py
@@ -0,0 +1,3 @@
+list_p='2022-11-10 04:38+05:00'
+list = list(list_p)
+print(''.join(list[10:16]))
